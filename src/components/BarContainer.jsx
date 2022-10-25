c886d56f2b877b74ab2f1692cc052a8f736b7e0d
ananttrivedi0709 <theananttrivedi@gmail.com>
2022-10-25 12:24:44 +0530

    Colored the labels

--- src/components/BarContainer.jsx
+++ src/components/BarContainer.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import shuffleArray from "../lib/shuffleArray";
 import Bar from "./Bar";
 import ResultContainer from "./ResultContainer";
 
@@ -15,25 +16,19 @@ const bars_intital = [
   { num: 10, currentlyLooking: false, correct: false },
 ];
 
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    // Generate random number
-    let j = Math.floor(Math.random() * (i + 1));
-
-    let temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-
-  return array;
-}
 function BarContainer() {
   // shuffleArray(bars)
   const [bars, setBars] = useState(shuffleArray(bars_intital));
   const [visualised, setVisualised] = useState(false);
   const [index, setIndex] = useState(null);
   const valueToBeSearched = 10;
+  function handleReset() {
+    setBars(shuffleArray(bars_intital));
+    setVisualised(false);
+    setIndex(null);
+  }
   async function visualizeLinearSearch() {
+    console.log("Playing: In Func Linear Search Visualise");
     // 1. Set the first to currentlyLooking = true
     // 2. then compare
     // 3. If true then break the loop and set correct = true
@@ -42,24 +37,38 @@ function BarContainer() {
     // Display: The value is not in the list
     let i = 0;
     let foundcorrect = false;
-    while (i <= bars.length) {
-      if (foundcorrect) {
-        setVisualised(true);
-        setIndex(i - 1);
-        break;
-      }
+    while (i < bars.length) {
       // 1. Set the current bar to looking = true
-      setBars((prevBars) => {
-        const newBars = [...prevBars];
-        if (i < bars.length) {
+      if (i < bars.length) {
+        setBars((prevBars) => {
+          const newBars = [...prevBars];
           const barToBeLookedAt = prevBars[i];
           barToBeLookedAt.currentlyLooking = true;
           newBars[i] = barToBeLookedAt;
-        }
-        return newBars;
-      });
+          return newBars;
+        });
+      }
 
-      // 2. Set the previous bar to looking = false
+      // 2. Check the condition if true then make it correct visually
+      //   if (bars[i].num === valueToBeSearched) {
+      //     setBars((prevBars) => {
+      //       const newBars = [...prevBars];
+      //       const barToBeMarkedCorrect = prevBars[i];
+      //       barToBeMarkedCorrect.currentlyLooking = false;
+      //       barToBeMarkedCorrect.correct = true;
+      //       newBars[i] = barToBeMarkedCorrect;
+      //       foundcorrect = true;
+      //       return newBars;
+      //     });
+      //   }
+
+      //   if (foundcorrect) {
+      //     setVisualised(true);
+      //     setIndex(i);
+      //     break;
+      //   }
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      // 3. Set the previous bar to looking = false
       setBars((prevBars) => {
         const newBars = [...prevBars];
         if (i != 0) {
@@ -70,66 +79,24 @@ function BarContainer() {
         return newBars;
       });
 
-      // 3. Check the condition if true then make it correct visually
-      setBars((prevBars) => {
-        const newBars = [...prevBars];
-        if (i < bars.length && prevBars[i].num === valueToBeSearched) {
-          const barToBeMarkedCorrect = prevBars[i];
-          barToBeMarkedCorrect.currentlyLooking = false;
-          barToBeMarkedCorrect.correct = true;
-          newBars[i] = barToBeMarkedCorrect;
-          foundcorrect = true;
-        }
-        return newBars;
-      });
-
-      if (foundcorrect) {
-        setVisualised(true);
-        setIndex(i - 1);
-      }
-
-      //   setBars((prevBars) => {
-      //     //   1. Set the previous bar to normal
-      //     //   2. Set the current bar to looking = true
-      //     //   3. Update the bars array
-      //     const newBars = [...prevBars];
-
-      //     //
-      //     if (i != 0) {
-      //       const barBefore = prevBars[i - 1];
-      //       barBefore.currentlyLooking = false;
-      //       newBars[i - 1] = barBefore;
-      //     }
-
-      //     if (i < bars.length) {
-      //       const barToBeLookedAt = prevBars[i];
-      //       barToBeLookedAt.currentlyLooking = true;
-      //       newBars[i] = barToBeLookedAt;
-      //     }
-
-      //     if (i < bars.length && prevBars[i].num === valueToBeSearched) {
-      //       const barToBeMarkedCorrect = prevBars[i];
-      //       barToBeMarkedCorrect.currentlyLooking = false;
-      //       barToBeMarkedCorrect.correct = true;
-      //       newBars[i] = barToBeMarkedCorrect;
-      //       foundcorrect = true;
-      //     }
-
-      //     return newBars;
-      //   });
-
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
       i++;
     }
   }
 
   useEffect(() => {
+    // if (!visualised) {
     visualizeLinearSearch();
-  }, []);
+    // }
+  }, [visualised]);
 
   return (
     <div className="flex flex-col gap-y-8">
+      <div
+        className="bg-yellow-400 w-fit px-4 font-bold cursor-pointer"
+        onClick={handleReset}
+      >
+        Reset
+      </div>
       <div className="flex gap-x-2">
         {bars.map((bar, index) => {
           const { num, currentlyLooking, correct } = bar;
--- src/components/ResultContainer.jsx
+++ src/components/ResultContainer.jsx
@@ -1,12 +1,24 @@
+import { ImCross, ImCheckmark } from "react-icons/im";
 const ResultContainer = ({ value, index, notCorrect }) => {
   let component = (
-    <p>
-      The value <span className="font-bold">{value}</span> found successfully at
-      index <span className="font-bold text-green-600">{index}</span>!
-    </p>
+    <div>
+      {/* <p>
+        The value <span className="font-bold">{value}</span> found successfully
+        at index <span className="font-bold text-green-600">{index}</span>!
+      </p> */}
+      <div className="flex items-center justify-center gap-x-4">
+        <p className="text-green-600 font-bold">Correct Value</p>
+        <ImCheckmark color="#16A34A" />
+      </div>
+    </div>
   );
   if (notCorrect) {
-    component = <p>Incorrect Value </p>;
+    component = (
+      <div className="flex items-center justify-center gap-x-4">
+        <p className="text-red-600 font-bold">Incorrect Value</p>
+        <ImCross color="#DC2626" />
+      </div>
+    );
   }
   return <div className="shadow text-center">{component}</div>;
 };
