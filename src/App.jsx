fd8764615eed9fbdcaa59e383b853d49dd595588
ananttrivedi0709 <theananttrivedi@gmail.com>
2022-10-26 18:26:16 +0530

    Added font: Inter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import BarContainer from "./components/BarContainer";
-
+import "./assets/Inter/fonts.css";
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center mt-8 overflow-x-hidden">
+      <div className="font-Inter flex flex-col items-center mt-8 overflow-x-hidden">
         <BarContainer />
       </div>
     </>
