4d92b61c4794548e1ad1dac1c391f007bdcc7c23
ananttrivedi0709 <theananttrivedi@gmail.com>
2022-10-30 00:42:10 +0530

    Changed width css

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="font-Inter px-2 pb-2 min-h-screen w-screen flex flex-col items-center mt-4 overflow-x-hidden">
+    <div className="font-Inter px-2 pb-2 min-h-screen flex flex-col items-center mt-4 overflow-x-hidden">
       <Outlet />
     </div>
   );
